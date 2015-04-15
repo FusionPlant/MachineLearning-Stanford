c5fa9704fd472557e73bffaa0cd2922dfc56c44c
FusionPlant <yetailai@gmail.com>
2015-04-15 14:56:42 -0700

    Regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = sum(-y.*log(sigmoid(X*theta))-(1-y).*log(1-sigmoid(X*theta)))/m+lambda/2/m*(theta'*theta-theta(1)^2);
 
+grad = X'*(sigmoid(X*theta)-y)/m+lambda/m*theta;
+
+grad(1)=grad(1)-lambda/m*theta(1);
 
 
 
