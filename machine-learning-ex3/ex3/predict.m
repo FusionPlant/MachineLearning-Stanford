12fbdc4a58925880955d676269f8be0758a1c248
FusionPlant <yetailai@gmail.com>
2015-04-16 01:36:41 -0700

    Neural Network

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,8 +21,10 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
+a1 = [ones(m, 1) X];
+a2 = sigmoid(a1*Theta1');
+a2 = [ones(size(a2,1),1) a2];
+[~, p]=max(sigmoid(a2*Theta2'), [], 2);
 
 
 
